847a4e295907d3e2d0ac16724eb424b403854bf7
Elchi DIana <47711461+daimondy@users.noreply.github.com>
2019-05-05 20:59:28 +0300

    ы

---
+++ server/static/file.js
@@ -0,0 +1,6 @@
+var foldBtns = document.getElementsByClassName("fold-button");
+for (var i = 0; i<foldBtns.length; i++){
+ foldBtns[i].addEventListener("click", function(event) {
+ console.log("you clicked ", event.target);
+ });
+}
